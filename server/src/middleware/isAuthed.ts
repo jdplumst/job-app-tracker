a97061fd709d6a340fb4201e5c521b265ffc4c35
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-04 17:53:02 -0400

    Add isAuthed middleware

---
+++ server/src/middleware/isAuthed.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function isAuthed(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.session.user) next();
+  else
+    return res.status(401).send("You are not authorized to make this request");
+}
--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,6 +1,11 @@
 import "express-session";
+export = session;
 declare module "express-session" {
   interface SessionData {
     user: { userId: string; username: string };
   }
+}
+
+interface SessionData {
+  cookie: Cookie;
 }
