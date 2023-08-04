700be73738523c95fb5625dc4cc4deccabe87429
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-04 14:22:52 -0400

    Set up express-session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,27 @@
 import express from "express";
 import "dotenv/config";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const app = express();
+
+app.use(
+  session({
+    name: "jat_sid",
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URL,
+      collectionName: "Session"
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+    }
+  })
+);
 
 app.get("/", function (req, res) {
   res.send("Hello World");
