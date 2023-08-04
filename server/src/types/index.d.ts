5d401e26f0bea18b31a5be2050d7f35abe1b6f10
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-04 18:03:23 -0400

    Add prisma as dependency

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,11 +1,7 @@
 import "express-session";
-export = session;
+
 declare module "express-session" {
   interface SessionData {
     user: { userId: string; username: string };
   }
-}
-
-interface SessionData {
-  cookie: Cookie;
 }
