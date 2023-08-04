606b9da6705a995404d8930916bd9ed2870c1007
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-04 01:41:18 -0400

    Install dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import "dotenv/config";
+
 const app = express();
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(5000);
+app.listen(process.env.PORT);
