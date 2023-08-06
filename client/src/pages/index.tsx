0ab3d220967e43aedfa210df1b32ee0ace18c943
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-06 17:56:46 -0400

    Add title and buttons to home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,7 +10,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className={`flex min-h-screen flex-col items-center justify-evenly`}></main>
+        className={`flex min-h-screen flex-col items-center justify-evenly`}>
+        <h1 className="text-8xl font-bold">Job Application Tracker</h1>
+        <div className="flex gap-10">
+          <button className="h-28 w-60 rounded-lg border-2 border-black bg-purple-500 p-4 text-4xl font-bold text-white hover:cursor-pointer hover:bg-purple-600">
+            Log In
+          </button>
+          <button className="h-28 w-60 rounded-lg border-2 border-black bg-green-500 p-4 text-4xl font-bold text-white hover:cursor-pointer hover:bg-green-600">
+            Sign Up
+          </button>
+          <button className="h-28 w-60 rounded-lg border-2 border-black bg-blue-500 p-4 text-4xl font-bold text-white hover:cursor-pointer hover:bg-blue-600">
+            Guest Login
+          </button>
+        </div>
+      </main>
     </>
   );
 }
