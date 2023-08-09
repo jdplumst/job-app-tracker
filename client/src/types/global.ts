15ab2ec1470173265acddd5a021a01dd388a444a
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-08 20:12:39 -0400

    Set up React Query

--- client/src/types/global.ts
+++ client/src/types/global.ts
@@ -1,1 +1,6 @@
+export const api =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_DEV_SERVER_URL
+    : process.env.NEXT_PUBLIC_PROD_SERVER_URL;
+
 export type Form = "login" | "register";
