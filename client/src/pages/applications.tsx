4ff307dd7385a75301e459ff2386b83724ac0938
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-11 08:43:54 -0400

    Push users to application page if logged in

--- client/src/pages/applications.tsx
+++ client/src/pages/applications.tsx
@@ -14,7 +14,7 @@ export default function Applications() {
 
   if (userLoading)
     return (
-      <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
         <LoadingSpinner />
       </div>
     );
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,31 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
 import Login from "@/components/Login";
 import Register from "@/components/Register";
+import useSession from "@/hooks/useSession";
 import { Form } from "@/types/global";
 import Head from "next/head";
+import Router from "next/router";
 import { useState } from "react";
 
 export default function Home() {
+  const { data: user, isLoading: userLoading } = useSession();
+  if (user && !userLoading) {
+    Router.push("/applications");
+  }
+
   const [form, setForm] = useState<Form>("login");
 
   const changeForm = (f: Form) => {
     setForm(f);
     return;
   };
+
+  if (user || userLoading)
+    return (
+      <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <LoadingSpinner />
+      </div>
+    );
 
   return (
     <>
