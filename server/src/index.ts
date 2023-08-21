5da759b33ae94a1dcedb289495fb12f79d07a5e3
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-21 11:48:30 -0400

    Modify cookie setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,6 +33,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
     }
   })
