5180b1d887deb2c2e6c5ee03573f302971d946aa
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-21 12:12:05 -0400

    Modify CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,7 @@ app.use(
       process.env.DEV_CLIENT_URL as string,
       process.env.PROD_CLIENT_URL as string
     ],
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
     credentials: true
   })
 );
@@ -36,7 +37,7 @@ app.use(
         process.env.NODE_ENV === "production"
           ? process.env.PROD_SERVER_URL
           : process.env.DEV_SERVER_URL,
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
     }
